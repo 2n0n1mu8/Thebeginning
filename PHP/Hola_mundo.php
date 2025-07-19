7b399da5fb49708843c2577aebc5cdf9accbd88e
2n0n1mu8 <anonymusamericadelsur@gmail.com>
2025-07-19 12:11:47 -0500

    main

--- PHP/Hola_mundo.php
+++ PHP/Hola_mundo.php
@@ -11,6 +11,9 @@ echo "entendio?";
 $hola = "hola para las variables es '$' y el nombre osea 'nombre'";
 echo $hola;
 */
+
+use function PHPSTORM_META\type;
+
 echo "Hola mundo\n";
 /*
 Es un comentario de varias lineas
@@ -23,5 +26,26 @@ solo es slash y asterisco
 $my_string = "Hola, soy una cadena de texto";
 $my_string = "Aqui cambio el valor de la cadena de texto";
 echo $my_string . "\n";
-echo gettype($my_string) . "\n"; // Muestra el tipo de dato de la variable
-echo $my_string; // Muestra el contenido de la variable
+echo gettype($my_string) . "\n";
+
+$my_string = 6;
+echo $my_string . "\n";
+echo gettype($my_string) . "\n";
+
+$my_string = "esto es una cadena de texto";
+
+$my_int = 10;
+$my_int = $my_int + 9;
+echo $my_int . "\n";
+echo $my_int - 2 . "\n";
+echo $my_int . "\n";
+echo gettype($my_int) . "\n";
+
+$my_double = 7.5;
+echo gettype($my_double) . "\n";
+echo $my_int + $my_double . "\n";
+echo $my_int . $my_int + $my_double /*$my_string*/ . "\n";
+
+$my_bool = true;
+echo $my_bool . "\n";
+echo gettype($my_bool) . "\n";
