4d7137c816150d8279421c0b872a8c36ab82440d
2n0n1mu8 <anonymusamericadelsur@gmail.com>
2025-07-19 11:09:33 -0500

    main

--- PHP/Hola_mundo.php
+++ PHP/Hola_mundo.php
@@ -1,4 +1,5 @@
 <?php
+/*
 echo "sin salto de linea\n >>";
 echo "Hola usuario";
 echo "como estas?<<\n";
@@ -9,10 +10,15 @@ echo "entendio?";
 
 $hola = "hola para las variables es '$' y el nombre osea 'nombre'";
 echo $hola;
-
+*/
+echo "Hola mundo";
 /*
 Es un comentario de varias lineas
 es facil
 solo es slash y asterisco
 */
 
+// Variables 
+
+$my_string = "Hola, soy una cadena de texto";
+echo $my_string;
