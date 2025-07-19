4bc0bb350e3242451b547f334c3a24f7ba052208
2n0n1mu8 <anonymusamericadelsur@gmail.com>
2025-07-19 10:43:32 -0500

    main

--- PHP/Hola_mundo.php
+++ PHP/Hola_mundo.php
@@ -1,3 +1,11 @@
 <?php
+echo "sin salto de linea\n >>";
 echo "Hola usuario";
-echo "como estas?";
+echo "como estas?<<\n";
+
+echo "con salto de linea \n";
+echo "se uliza barra invertida o diagonal inversa --> '\' \n";
+echo "entendio?";
+
+$hola = "hola para las variables es '$' y el nombre osea 'nombre'";
+echo $hola;
